e41770d3208c8eff44ec45041ca63529529a72fe
Khoi Nguyen <khoi.fish084@gmail.com>
2017-06-30 20:50:11 -0700

    deploy

--- app/components/children/Features.js
+++ app/components/children/Features.js
@@ -12,31 +12,30 @@ class Features extends React.Component {
                     <div className="container">
                         <div className="col-md-6 col-md-push-6">
                             <figure className="fh5co-feature-image animate-box">
-                                <img src="images/macbook.png" alt="Free HTML5 Bootstrap Template by FREEHTML5.co"/>
+                                <img src="images/macbook.png"/>
                             </figure>
                         </div>
                         <div className="col-md-6 col-md-pull-6">
-                            <span className="fh5co-label animate-box">See Features</span>
-                            <h2 className="fh5co-lead animate-box">Superb Features</h2>
+                            <h2 className="fh5co-lead animate-box">Features</h2>
                             <div className="fh5co-feature">
                                 <div className="fh5co-icon animate-box"><i className="icon-check2"></i></div>
                                 <div className="fh5co-text animate-box">
-                                    <h3>Beautiful Typography</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                                    <h3>Virtual Reality</h3>
+                                    <p>Gain full access to the virtual reality library. Enhance your mindful experience by taking your mind to your virtual 'happy place.'</p>
                                 </div>
                             </div>
                             <div className="fh5co-feature">
                                 <div className="fh5co-icon animate-box"><i className="icon-check2"></i></div>
                                 <div className="fh5co-text animate-box">
-                                    <h3>Eco Friendly</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                                    <h3>Point System</h3>
+                                    <p>Earn points as you develop your mindfulness abilities to unlock more content.</p>
                                 </div>
                             </div>
                             <div className="fh5co-feature">
                                 <div className="fh5co-icon animate-box"><i className="icon-check2"></i></div>
                                 <div className="fh5co-text animate-box">
-                                    <h3>Wide and Boxed</h3>
-                                    <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+                                    <h3>Dashboard</h3>
+                                    <p>Track your progress, check the weather, schedule events, access audio and V.R. libaries and more.</p>
                                 </div>
                             </div>
 
--- app/components/children/Section1.js
+++ app/components/children/Section1.js
@@ -12,68 +12,44 @@ class Section1 extends Component {
         <div className="container">
           <div className="row row-bottom-padded-md">
             <div className="col-md-6 col-md-offset-3 text-center">
-              <h2 className="fh5co-lead animate-box">Our Products</h2>
-              <p className="fh5co-sub-lead animate-box">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+              <h2 className="fh5co-lead animate-box">What is Mindfulness?</h2>
+              <p className="fh5co-sub-lead animate-box">“Mindfulness is the basic human ability to be fully present, aware of where we are and what we’re doing, and not overly reactive or overwhelmed by what’s going on around us.” </p>
             </div>
           </div>
           
           <div className="row">
             <div className="col-md-4 col-sm-6 col-xxs-12 animate-box">
-              <a href="images/img_1.jpg" className="fh5co-project-item image-popup">
-                <img src="images/img_1.jpg" alt="Image"/>
+              <a href="images/mindfulness-workplace.jpg" className="fh5co-project-item image-popup">
+                <img src="images/mindfulness-workplace.jpg" alt="Image"/>
                 <div className="fh5co-text">
-                  <h2>Beautiful Sunrise</h2>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                  <h2>"I've found some much needed peace."</h2>
+                  <p><q>For the longest time work has been one of, if not the main source of stress in my life. But mindfulness has helped reduce that stress and even increased my productivity in the workplace.</q> - Tiffany Evans</p>
                 </div>
               </a>
             </div>
             <div className="col-md-4 col-sm-6 col-xxs-12 animate-box">
-              <a href="images/img_1.jpg" className="fh5co-project-item image-popup">
-                <img src="images/img_1.jpg" alt="Image"/>
+              <a href="images/happy-family.jpg" className="fh5co-project-item image-popup">
+                <img src="images/happy-family.jpg" alt="Image"/>
                 <div className="fh5co-text">
-                  <h2>Beautiful Sunrise</h2>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                  <h2>"It's been a wonderful learning experience."</h2>
+                  <p><q>"My wife and I were looking for new ways to strengthen our relationship. With mindfulness, we've been able to identify some of our weak points and focus on resolving them. It's been a wonderful learning experience." - James Pilsen</q></p>
                 </div>
               </a>
             </div>
             <div className="col-md-4 col-sm-6 col-xxs-12 animate-box">
-              <a href="images/img_1.jpg" className="fh5co-project-item image-popup">
-                <img src="images/img_1.jpg" alt="Image"/>
+              <a href="images/how-to-reduce-job-stress.png" className="fh5co-project-item image-popup">
+                <img src="images/how-to-reduce-job-stress.png" alt="Image"/>
                 <div className="fh5co-text">
-                  <h2>Beautiful Sunrise</h2>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-                </div>
-              </a>
-            </div>
-            <div className="col-md-4 col-sm-6 col-xxs-12 animate-box">
-              <a href="images/img_1.jpg" className="fh5co-project-item image-popup">
-                <img src="images/img_1.jpg" alt="Image"/>
-                <div className="fh5co-text">
-                  <h2>Beautiful Sunrise</h2>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-                </div>
-              </a>
-            </div>
-            <div className="col-md-4 col-sm-6 col-xxs-12 animate-box">
-              <a href="images/img_1.jpg" className="fh5co-project-item image-popup">
-                <img src="images/img_1.jpg" alt="Image"/>
-                <div className="fh5co-text">
-                  <h2>Beautiful Sunrise</h2>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
-                </div>
-              </a>
-            </div>
-            <div className="col-md-4 col-sm-6 col-xxs-12 animate-box">
-              <a href="images/img_1.jpg" className="fh5co-project-item image-popup">
-                <img src="images/img_1.jpg" alt="Image"/>
-                <div className="fh5co-text">
-                  <h2>Beautiful Sunrise</h2>
-                  <p>Far far away, behind the word mountains, far from the countries Vokalia..</p>
+                  <h2>"Balance has been the key."</h2>
+                  <p><q>Well, I'm a full-time mother and student. It seemed like I was on the verge of insanity every day. When my girlfriend introduced me to mindfulness I have to admit I was pretty skeptical about it. Of course the headaches are still there, but over time, I've definitely noticed a difference.</q> - Christine Thatcher</p>
                 </div>
               </a>
             </div>
           </div>
-          
+
+          <div className="embed-responsive embed-responsive-16by9">
+              <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/3nwwKbM_vJc" frameBorder="0" allowFullScreen></iframe>
+          </div>
           
         </div>
       </section>
