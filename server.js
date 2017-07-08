4ea595fefbbd52b16955182a5f52c3ef3d886c64
hanifabo <hanifabo@gmail.com>
2017-07-07 19:22:50 -0700

    Adding todays changes here

--- server.js
+++ server.js
@@ -15,21 +15,20 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 app.use("/", routes);
 
-var db = process.env.MONGODB_URI || "mongodb://localhost/mindfulApp";
+var db = process.env.MONGODB_URI || "mongodb://hanifa:can123@ds127730.mlab.com:27730/mindful";
 
 // Connect mongoose to our database
 mongoose.connect(db, function(error) {
-  // Log any errors connecting with mongoose
-  if (error) {
-    console.error(error);
-  }
-  // Or log a success message
-  else {
-    console.log("mongoose connection is successful");
-  }
-});
-
-// Start the server
-app.listen(PORT, function() {
-  console.log("Now listening on port %s! Visit localhost:%s in your browser.", PORT, PORT);
+    // Log any errors connecting with mongoose
+    if (error) {
+        console.error(error);
+    }
+    // Or log a success message
+    else {
+        console.log("mongoose connection is successful");
+        // Start the server
+        app.listen(PORT, function() {
+            console.log("Now listening on port %s! Visit localhost:%s in your browser.", PORT, PORT);
+        });
+    }
 });
